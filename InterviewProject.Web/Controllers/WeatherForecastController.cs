f097750ceb411af0abb9853b2b3fe5588948a2cb
Alejandro Tapia <a.tapiat@pucp.pe>
2024-08-31 16:38:25 -0500

    [FEATURE] Conexión con el API de AccuWeather para obtener la locación y 5 días del forecast

--- InterviewProject.Web/Controllers/WeatherForecastController.cs
+++ InterviewProject.Web/Controllers/WeatherForecastController.cs
@@ -4,36 +4,40 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using InterviewProject.Models;
+using InterviewProject.Services;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json.Nodes;
 
 namespace InterviewProject.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class WeatherForecastController(ILogger logger, IConfiguration configuration) : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
+        private static readonly string[] Summaries = [
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        ];
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+        private readonly ILogger _logger = logger;
+        private readonly IConfiguration _configuration = configuration;
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        [Route("GetFiveDayForecast")]
+        public async Task<IActionResult> GetFiveDayForecast([FromQuery(Name = "city")] string cityName)
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            AccuWeatherService accuWeather = new(_configuration, _logger);
+            string location = await accuWeather.GetLocationAsync(cityName);
+            if (string.IsNullOrWhiteSpace(location))
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                return BadRequest($"No se pudo encontrar la locación {cityName}");
+            }
+            JsonArray forecast = await accuWeather.GetFiveDayForecastByLocation(location);
+            if (forecast == null)
+            {
+                return BadRequest($"No se pudo obtener la información del tiempo para 5 día para la locación {cityName}");
+            }
+            return Ok(forecast);
         }
     }
 }
---
+++ InterviewProject.Web/Services/AccuWeatherService.cs
@@ -0,0 +1,74 @@
+﻿using InterviewProject.Controllers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace InterviewProject.Services
+{
+    public class AccuWeatherService
+    {
+        private static HttpClient _client;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger _logger;
+
+        public AccuWeatherService(IConfiguration configuration, ILogger logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri("http://dataservice.accuweather.com/")
+            };
+        }
+
+        public async Task<string> GetLocationAsync(string location)
+        {
+            string path = _configuration.GetValue<string>("UrlPath:Location");
+            string apikey = _configuration.GetValue<string>("AuthKey");
+            string lang = _configuration.GetValue<string>("DefaultLanguage");
+
+            HttpResponseMessage response = await _client.GetAsync($"{path}?apikey={apikey}&q={location}&language={lang}");
+            if (response.IsSuccessStatusCode)
+            {
+                try
+                {
+                    JsonArray jsonResponse =  (JsonArray)JsonNode.Parse(await response.Content.ReadAsStringAsync());
+                    return jsonResponse.First()["Key"].ToString();
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error al obtener la llave de una locación.");
+                    return null;
+                }
+            }
+            return null;
+        }
+
+        public async Task<JsonArray> GetFiveDayForecastByLocation(string locationKey)
+        {
+            string path = _configuration.GetValue<string>("UrlPath:ForecastFiveDay");
+            string apikey = _configuration.GetValue<string>("AuthKey");
+            string lang = _configuration.GetValue<string>("DefaultLanguage");
+
+            HttpResponseMessage response = await _client.GetAsync($"{path}/{locationKey}?apikey={apikey}&language={lang}");
+            if (response.IsSuccessStatusCode)
+            {
+                try
+                {
+                    JsonObject forecast = (JsonObject)JsonNode.Parse(await response.Content.ReadAsStringAsync());
+                    return (JsonArray)forecast["DailyForecasts"];
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error al obtener la data del tiempo para 5 días.");
+                    return null;
+                }
+            }
+            return null;
+        }
+    }
+}
